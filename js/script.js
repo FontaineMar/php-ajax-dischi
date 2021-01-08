4523ffce48defddbee08283eadc107b60df91656
marco <odri.fo@gmail.com>
2021-01-08 17:03:46 +0100

    finito

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ const myApp = new Vue({
   el:'#root',
   data:{
     musicList:[],
-    url:'http://localhost:8888/esercizi-php/php-ajax-dischi/database.php',
+    url:'http://localhost:8888/esercizi-php/php-ajax-dischi/php/database.php',
     jazzMusic:[],
     metalMusic:[],
     rockMusic:[],
     popMusic:[]
   },
   mounted:function(){
-    axios.get('http://localhost:8888/esercizi-php/php-ajax-dischi/database.php')
+    axios.get('http://localhost:8888/esercizi-php/php-ajax-dischi/php/database.php')
       .then((music)=> {
         let {response} = music.data
       this.musicList.push(...response)
